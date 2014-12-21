2762b7e335e390b64d570def315913d0889b8956
UNA A. KRAVETS <uakravet@us.ibm.com>
2014-12-20 23:59:49 -0600

    page turning red on request so thats cool

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -4,6 +4,24 @@ chrome.runtime.onInstalled.addListener(function (details) {
   console.log('previousVersion', details.previousVersion);
 });
 
-chrome.browserAction.setBadgeText({text: '\'Allo'});
+chrome.browserAction.setBadgeText({text: 'Hi'});
 
 console.log('\'Allo \'Allo! Event Page for Browser Action');
+
+var bgColor = '#c69';
+
+function changeBg(bgColor) {
+  var myElement = document.querySelector("body");
+  myElement.style.backgroundColor = bgColor;
+};
+
+changeBg(bgColor);
+
+// Called when the user clicks on the browser action.
+chrome.browserAction.onClicked.addListener(function(tab) {
+  // No tabs or host permissions needed!
+  console.log('Turning ' + tab.url + ' red!');
+  chrome.tabs.executeScript({
+    code: 'document.body.style.backgroundColor="red"'
+  });
+});
