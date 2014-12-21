d13be87e253a4cfcb4908a020de65ebdd3f4a8f7
UNA A. KRAVETS <uakravet@us.ibm.com>
2014-12-21 00:20:48 -0600

    scripts file

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -4,24 +4,14 @@ chrome.runtime.onInstalled.addListener(function (details) {
   console.log('previousVersion', details.previousVersion);
 });
 
-chrome.browserAction.setBadgeText({text: 'Hi'});
+chrome.browserAction.setBadgeText({text: 'Off'});
 
 console.log('\'Allo \'Allo! Event Page for Browser Action');
 
-var bgColor = '#c69';
-
-function changeBg(bgColor) {
-  var myElement = document.querySelector("body");
-  myElement.style.backgroundColor = bgColor;
-};
-
-changeBg(bgColor);
 
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
-  // No tabs or host permissions needed!
-  console.log('Turning ' + tab.url + ' red!');
-  chrome.tabs.executeScript({
-    code: 'document.body.style.backgroundColor="red"'
-  });
+  //console.log('Turning ' + tab.url + "red!");
+  chrome.tabs.executeScript(null, {file: "myScript.js"});
+  chrome.browserAction.setBadgeText({text: 'On'});
 });
---
+++ app/scripts/myScript.js
@@ -0,0 +1,16 @@
+// var bgColor = '#c69',
+//     fontSize = 20;
+
+// function changeBg(bgColor) {
+//   var myElement = document.querySelector("body");
+//   myElement.style.backgroundColor = bgColor;
+// };
+
+// function changeFontSize(fontSize) {
+//     var myElement = document.querySelector("body");
+//     myElement.style.fontSize = fontSize + "px";
+// }
+
+// changeBg(bgColor);
+
+alert('hi');
